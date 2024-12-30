f7dfa387afca8d8c34c73d2796370b9f23df67ad
Conor McNamara <conormcnamara245@gmail.com>
2024-12-30 22:37:49 +0000

    Add React routes

--- app/backend/src/api/v1/routes/messages.py
+++ app/backend/src/api/v1/routes/messages.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 
-router = APIRouter(prefix="/message")
+router = APIRouter(prefix="/message", tags=["Messages"])
 
 
 @router.get("/")
--- app/frontend/src/App.tsx
+++ app/frontend/src/App.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './pages/Home/HomePage';
 
 const App: React.FC = () => {
   return (
-    <div>
-      <HomePage />
-    </div>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
