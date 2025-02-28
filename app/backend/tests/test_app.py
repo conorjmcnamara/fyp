3432709389a83db4d10dbcb9795ad6ea7dfc0da6
Conor McNamara <conormcnamara245@gmail.com>
2025-02-28 21:02:33 +0000

    Add pytest fixture

--- app/backend/tests/test_app.py
+++ app/backend/tests/test_app.py
@@ -1,7 +1,13 @@
+import pytest
 from fastapi.testclient import TestClient
 from src.app import app
 
 client = TestClient(app)
+
+
+@pytest.fixture(autouse=True)
+def mock_env_variables(monkeypatch: pytest.MonkeyPatch) -> None:
+    monkeypatch.setenv("DATABASE_URL", "sqlite:///test.db")
 
 
 def test_app():
